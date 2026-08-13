3ed02fb714fab8c1ceb1ffaeab2471a5637aba90
Rasajna <rasajnasristi9@gmail.com>
2026-08-14 00:12:35 +0530

    Connecting backend and frontend together with the new URLs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     setAnswer("");
 
     try {
-      const response = await fetch("http://localhost:8000/ask", {
+      const response = await fetch("https://askmynotes-backend-r1bw.onrender.com", {
         method: "POST",
 
         headers: {
