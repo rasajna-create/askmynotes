bedddde154f52e7518e9db8c477788b2811f3a7e
Rasajna <rasajnasristi9@gmail.com>
2026-08-14 00:43:42 +0530

    Optimizing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     setAnswer("");
 
     try {
-      const response = await fetch("https://askmynotes-backend-r1bw.onrender.com/ask", {
+      const response = await fetch("https://askmynotes-backend-webservice.onrender.com/ask", {
         method: "POST",
 
         headers: {
